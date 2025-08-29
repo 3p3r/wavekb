f3feb7e5e9109785c552aa90bc3179e0bc1c1768
Sepehr Laal <sepehr.laal@gmail.com>
2025-08-28 18:45:38 -0700

    feat: docker compose generation alongside cdk infra for local dev

--- aws/stack.ts
+++ aws/stack.ts
@@ -1,8 +1,9 @@
 import * as cdk from "aws-cdk-lib";
 import { NextJSApp } from "./nextjs";
+import { Project } from "docker-compose-cdk";
 
 export class BackendStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps & { dockerCompose: Project }) {
     super(scope, id, props);
 
     new NextJSApp(this, "NextJSApp");
