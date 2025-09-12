2a8916d57c8ba904535974c3fe740c9a49cb5ade
Sepehr Laal <sepehr.laal@gmail.com>
2025-09-12 10:09:18 -0700

    feat: remult integration into the stack

---
+++ app/src/api.ts
@@ -0,0 +1,5 @@
+import { remultApi } from 'remult/remult-next'
+
+export const api = remultApi({
+    admin: true, // todo: disable in production
+})
---
+++ app/src/app/api/[...remult]/route.ts
@@ -0,0 +1,6 @@
+import { api } from "../../../api";
+
+// This file serves as a catch-all route for the Next.js API,
+// handling all API requests by routing them through Remult.
+
+export const { POST, PUT, DELETE, GET } = api;
